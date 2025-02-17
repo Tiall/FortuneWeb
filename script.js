8a12cf2c61fc46d0367c2935be216567c5a2cf4d
Luke Lanning <70123439+Tiall@users.noreply.github.com>
2025-02-17 14:35:29 -0600

    Update script.js
    Added comments

--- script.js
+++ script.js
@@ -5,18 +5,24 @@ var contentBox = document.getElementById("contentBox");
 window.addEventListener("load", loadTarotJson());
 
 async function loadTarotJson() {
+    // Fetch tarot card stuff if we don't have it already
     if (tarotCardSuits == null) {
         const response = await fetch('./tarotMeanings.json');
         const tarotJson = await response.json();
         tarotCardSuits = tarotJson;
     }
+    
+    // Do the front-end loading of the data
     loadTarot();
 }
 
 function loadTarot() {
+    // Set contentBox if it is not assigned already
     if (contentBox == null) {
         contentBox = document.getElementById("contentBox");
     }
+
+    // Attempt to load tarot card JSON data
     try{
         contentBox.innerHTML = JSON.stringify(tarotCardSuits[0].cards[0]);
     }
