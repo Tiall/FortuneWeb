f91da74c4338df989282454c8eb8f91898844539
LUKE LANNING <pacman4021@outlook.com>
2025-02-17 21:16:45 -0600

    Got tarot drawing to work and added gitignore

--- script.js
+++ script.js
@@ -5,15 +5,30 @@ var contentBox = document.getElementById("contentBox");
 window.addEventListener("load", loadTarotJson());
 
 async function loadTarotJson() {
-    // Fetch tarot card stuff if we don't have it already
-    if (tarotCardSuits == null) {
-        const response = await fetch('./tarotMeanings.json');
-        const tarotJson = await response.json();
-        tarotCardSuits = tarotJson;
+    // Attempt to load tarot card JSON data
+    try {
+        // Fetch tarot card stuff if we don't have it already
+        if (tarotCardSuits == null) {
+            const response = await fetch('./tarotMeanings.json');
+            const tarotJson = await response.json();
+            tarotCardSuits = tarotJson;
+        }
+
+    }
+    catch {
+        contentBox.innerHTML = "ERROR TarotCardsJSON not loaded!";
+        console.log("ERROR TarotCardsJSON not loaded!");
     }
     
     // Do the front-end loading of the data
     loadTarot();
+}
+
+function generateTarotCard() {
+    let randSuit = Math.floor(Math.random() * (tarotCardSuits.length));
+    let randCardIndex = Math.floor(Math.random() * (tarotCardSuits[randSuit].cards.length - 1));
+
+    return tarotCardSuits[randSuit].cards[randCardIndex];
 }
 
 function loadTarot() {
@@ -22,22 +37,22 @@ function loadTarot() {
         contentBox = document.getElementById("contentBox");
     }
 
-    // Attempt to load tarot card JSON data
-    try{
-        let randCard = tarotCardSuits[Math.floor(Math.random() * (tarotCardSuits.length + 1))].cards[Math.floor(Math.random() * (tarotCardSuits.cards.length + 1))]
-        contentBox.innerHTML = JSON.stringify(tarotCardSuits[0].cards[0]);
-        let cardNumBox = document.getElementsByClassName("tarotCard")[0].getElementsByClassName("cardNum")[0];
-        let cardDescBox = document.getElementsByClassName("tarotCard")[0].getElementsByClassName("cardDesc")[0];
-        let cardNameBox = document.getElementsByClassName("tarotCard")[0].getElementsByClassName("cardName")[0];
+    let randCard = generateTarotCard()
+    console.log(JSON.stringify(randCard));
 
-        cardNumBox.innerHTML = randCard.number;
-        cardDescBox.innerHTML = (Math.random() > 0.5) ? randCard.upright : randCard.reversed;
-        cardNameBox.innerHTML = randCard.name;
-        
-    }
-    catch {
-        contentBox.innerHTML = "ERROR TarotCardsJSON not loaded!";
-        print("ERROR TarotCardsJSON not loaded!");
-    }
+    // Set textbox to raw JSON data of the randomly drawn tarot card
+    contentBox.innerHTML = JSON.stringify(randCard);
+
+    // Get the data from the randomly drawn tarot card into their respective places
+    let cardNumBox = document.getElementsByClassName("tarotCard")[0].getElementsByClassName("cardNum")[0];
+    let cardDescBox = document.getElementsByClassName("tarotCard")[0].getElementsByClassName("cardDesc")[0];
+    let cardNameBox = document.getElementsByClassName("tarotCard")[0].getElementsByClassName("cardName")[0];
+
+    // Set the tarot card graphic's textboxes with the data from the randomly drawn tarot card
+    cardNumBox.innerHTML = randCard.number;
+    cardDescBox.innerHTML = (Math.random() > 0.5) ? randCard.upright : randCard.reversed;
+    cardNameBox.innerHTML = randCard.name;
+    
+    
     
 }
